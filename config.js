19ece043d71fd70ff6a7f58e46ba5e7128c38895
amaan-ahmad <amaanahmad582001@gmail.com>
2021-06-18 16:47:42 +0530

    refractoring

---
+++ config.js
@@ -0,0 +1,8 @@
+const marked = require('marked');
+const TerminalRenderer = require('marked-terminal');
+marked.setOptions({
+  // Define custom renderer
+  renderer: new TerminalRenderer()
+});
+
+module.exports = { marked };
--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const marked = require('marked');
-const TerminalRenderer = require('marked-terminal');
 const contentMD = require('./content.js');
-marked.setOptions({
-  // Define custom renderer
-  renderer: new TerminalRenderer()
-});
-
+const { marked } = require('./config.js');
 
 // Show the parsed data
 console.log(marked(contentMD));
