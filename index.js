0d1e71c20fac4542099445099d69865258584be6
Amaan Ahmad <amaanahmad582001@gmail.com>
2021-06-23 21:41:49 +0530

    welcoming color change

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const hashnode = require("./values/art");
 
 console.clear();
 
-vorpal.log(chalk.blue(hashnode));
+vorpal.log(chalk.blueBright(hashnode));
 
-vorpal.log(chalk.bgBlue.white.bold("--- Welcome to Hashnode easy ---"));
+vorpal.log(chalk.bgBlueBright.white.bold("--- Welcome to Hashnode easy ---"));
 
 vorpal.command("read", "enables reading").action(function (args, cb) {
   menu();
