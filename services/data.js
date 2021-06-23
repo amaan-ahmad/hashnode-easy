53a6f0b5ecb681df540fed857cc7c1e6933ab06d
Amaan Ahmad <amaanahmad582001@gmail.com>
2021-06-23 16:03:13 +0530

    service added for townhall articles

--- services/data.js
+++ services/data.js
@@ -21,6 +21,25 @@ async function getStories(type) {
     .catch(console.error);
 }
 // https://hashnode.com/ajax/post/lets-talk-with-mays-top-5-hashnode-authors-ckq0jiixv05nc5ws1frwk181x
+// https://hashnode.com/ajax/user/publication-entries?publication=5d381a3bd453ac336e478173&skip=5&limit=6
+
+async function getTownHallStories() {
+  const url = `https://hashnode.com/ajax/user/publication-entries?publication=5d381a3bd453ac336e478173&limit=6`;
+  return fetch(url)
+    .then((res) => res.text())
+    .then(JSON.parse)
+    .then((res) => {
+      const result = [];
+      res.posts.forEach(({ slug, title, cuid }) => {
+        result.push({
+          slug,
+          title,
+          cuid,
+        });
+      });
+      return result;
+    });
+}
 
 async function getPostContent(slug, cuid) {
   const url = `https://hashnode.com/ajax/post/${slug}-${cuid}`;
@@ -33,4 +52,4 @@ async function getPostContent(slug, cuid) {
     .catch(console.error);
 }
 
-module.exports = { getStories, getPostContent };
+module.exports = { getStories, getPostContent, getTownHallStories };
